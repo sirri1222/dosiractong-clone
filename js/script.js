3c22387cdbba2c037bdc64258df08e847ebeebba
sirri1222 <hyeonji159753@gmail.com>
2023-01-10 19:58:27 +0900

    비지니스 영역 스와이프 완료

--- js/script.js
+++ js/script.js
@@ -168,4 +168,27 @@ window.onload = function () {
       swiper.slideTo(index, 0, false);
     });
   });
+
+
+  // business slide
+  const swBusiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+     
+      },
+      1024: {
+        slidesPerView: 3,
+     
+      },
+    },
+  });
+
+
+  const businessModal = document.querySelector(".business-modal");
+  businessModal.addEventListener("click", function(){
+    businessModal.style.display="none"
+  })
 };
